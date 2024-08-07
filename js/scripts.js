c228d58f2fc9c0b4f67d1ace6cfc2d20f32410c9
unknown <ricardodonado1105@gmail.com>
2024-08-06 20:38:53 -0500

    buttons now toggles each section

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ const monthlyHours = document.getElementsByClassName("Monthly")
 
 
 let clickedButton = 1
+
 for(let i=0; i<timeSpanButtonElements.length;i++){
     timeSpanButtonElements[i].addEventListener("click",()=>{
         for(let i=0; i<timeSpanButtonElements.length;i++){
@@ -15,6 +16,16 @@ for(let i=0; i<timeSpanButtonElements.length;i++){
          }
          timeSpanButtonElements[i].classList.add("focused")
          clickedButton = i
-
+         let displayedDivs = new Array(i).fill(0)
+         for(let i=0; i<infoContainerElements.length;i++){
+            const selectedButton = [...infoContainerElements[i].getElementsByClassName("container")[0].getElementsByClassName("selectedPeriod")]
+            displayedDivs[i] = selectedButton[clickedButton]
+            selectedButton.forEach((button, index) =>{
+                if(index != clickedButton){
+                    button.classList.remove("displayed")
+                }
+            })
+            selectedButton[clickedButton].classList.add("displayed")
+        }
     })
 }
