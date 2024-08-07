869b2dad21a08f9c810a8c1446f8f918a57cbcaa
unknown <ricardodonado1105@gmail.com>
2024-08-06 19:45:05 -0500

    toggling buttons logic done

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
-const timeSpanButtonElements = document.getElementsByClassName("timeSpanButton")
+const timeSpanButtonElements = [...document.getElementsByClassName("timeSpanButton")]
 const infoContainerElements = document.getElementsByClassName("infoBox")
 const dailyHours = document.getElementsByClassName("Daily")
 const weeklyHours = document.getElementsByClassName("Weekly")
 const monthlyHours = document.getElementsByClassName("Monthly")
+
+
+let clickedButton = 1
+for(let i=0; i<timeSpanButtonElements.length;i++){
+    timeSpanButtonElements[i].addEventListener("click",()=>{
+        for(let i=0; i<timeSpanButtonElements.length;i++){
+            if(timeSpanButtonElements[i].classList.contains("focused")){
+                timeSpanButtonElements[i].classList.remove("focused")
+            }
+         }
+         timeSpanButtonElements[i].classList.add("focused")
+         clickedButton = i
+
+    })
+}
